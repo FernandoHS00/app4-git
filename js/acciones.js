d6e8f8ed284ecccf8d32c902155458534dd735b5
FernandoHS00 <hernandezsanchezfernando3h@gmail.com>
2017-04-25 08:05:33 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -20,7 +20,7 @@ $(document).ready(function (e){
 		escribehistoria ('La app se desconecto de la red');
 	},false);//se desconecto
 	
-	},flse);//ready device
+	},false);//ready device
 	
 });//document
 
